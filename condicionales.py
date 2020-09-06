895155ec619a72e3c6b5eebf953e388deb28c1d9
Dian Delph <da.dnvl@live.com>
2020-09-05 20:19:24 -0500

    Agrega ejercicio de condicionales

---
+++ condicionales.py
@@ -0,0 +1,12 @@
+#dice si es mayor de edad o no
+
+edad = int(input('Escribe tu edad: '))
+
+# pregunta si edad es mayor o igual a 18
+if edad >= 18:
+	#si sí, entonces la persona es mayor de edad
+	print('Eres mayor de edad')
+#si no
+else:
+	#es menor de edad
+	print('Eres menor de edad')
